5140778a8159ac5a0495f16a8c2c06fa161505c6
Sage Abdullah <sage.abdullah@torchbox.com>
2023-07-11 15:36:18 +0100

    Fix ESLint errors

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,10 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'react/jsx-filename-extension': [1, { extensions: ['.jsx', '.tsx'] }],
+    'func-names': 'off',
+    'no-param-reassign': 'off',
+    'prefer-arrow-callback': 'off',
+    'vars-on-top': 'off',
   },
   settings: {
     'import/resolver': {
@@ -21,4 +25,13 @@ module.exports = {
   // ESlint default behaviour ignores file/folders starting with "."
   // https://github.com/eslint/eslint/issues/10341
   ignorePatterns: ['!.*', 'node_modules', 'dist'],
+  overrides: [
+    {
+      files: [
+        'docs/_static/**',
+        'wagtail_modeladmin/static_src/wagtailmodeladmin/js/prepopulate.js',
+      ],
+      globals: { $: 'readonly', jQuery: 'readonly' },
+    },
+  ],
 };
